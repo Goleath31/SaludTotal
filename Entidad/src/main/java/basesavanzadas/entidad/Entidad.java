26fb1c7b2ada78f6d9489db0880f3f2fdd0689a2
Goleath31 <149979774+Goleath31@users.noreply.github.com>
2026-06-24 20:57:09 -0700

    inicio
    negocios, presentacion, entidad , persistencia

---
+++ Entidad/src/main/java/basesavanzadas/entidad/Entidad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package basesavanzadas.entidad;
+
+/**
+ *
+ * @author golea
+ */
+public class Entidad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
